1d9a288b7c988afbf7427025d967a7de6e140513
Vadim Dvukhzhilov <v.dvukhzhilov@tinkoff.ru>
2022-06-22 11:14:11 +0300

    feat: add alphabet letters

--- constants/alphabet.ts
+++ constants/alphabet.ts
@@ -24,6 +24,14 @@ export const RU_TO_GE = {
   "к'": 'ქ',
   п: 'პ',
   "п'": 'ფ',
+  "х'": 'ჰ',
+  "ц'": 'ც',
+  "ч'": 'ჩ',
+  "дз'": 'ძ',
+  "ч": 'ჭ',
+  "горл": 'ყ',
+  "ц": 'წ',
+  "дж": 'ჯ',
 } as const;
 
 export const RU_LETTERS = Object.keys(RU_TO_GE) as (keyof typeof RU_TO_GE)[];
